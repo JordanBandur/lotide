e18cc3d1924eb7fa5612988c2722cfc1153be65c
JordanBandur <jordanbandur@hotmail.ca>
2024-04-01 21:01:36 -0700

    Added more test cases

--- assertEqual.js
+++ assertEqual.js
@@ -10,5 +10,7 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Bootcamp", 1);
 assertEqual(1, 1);
 assertEqual(1, 2);
+assertEqual(1, "1");
