9aef35fc8dbacdf60ce098abaf21ef5aa8f3587e
JordanBandur <jordanbandur@hotmail.ca>
2024-04-03 14:08:08 -0700

    Remove redundancy

---
+++ flatten.js
@@ -0,0 +1,25 @@
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const flatten = function(arr) {
+  
+};
+
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
