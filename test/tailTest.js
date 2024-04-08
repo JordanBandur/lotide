e15280598a77d4dc8796be51591ffc78fad45ee6
JordanBandur <jordanbandur@hotmail.ca>
2024-04-08 13:33:36 -0700

    Refactor tailTest to use chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,26 +1,21 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it(`returns ["LightHouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-const result2 = tail(["Bye", "For", "Now", "Lighthouse", "Labs"]);
-assertEqual(result2.length, 4);
-assertEqual(result2[0], "For");
-assertEqual(result2[1], "Now");
-assertEqual(result2[2], "Lighthouse");
-assertEqual(result2[3], "Labs");
+  it(`returns ["For", "Now", "Lighthouse", "Labs"] for ["Bye", "For", "Now", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Bye", "For", "Now", "Lighthouse", "Labs"]), ["For", "Now", "Lighthouse", "Labs"]);
+  });
 
-const result3 = tail(["Hello"]);
-assertEqual(result3.length, 0);
-assertEqual(result3[0], undefined);
+  it(`returns empty array for ["Hello"]`, () => {
+    assert.deepEqual(tail(["Hello"]), []);
+  });
 
-const result4 = tail([]);
-assertEqual(result4.length, 0);
-assertEqual(result4[0], undefined);
+  it(`returns empty array for []`, () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+});
