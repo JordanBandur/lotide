e75c1151387483150b35d3edd11637ca46ea7906
JordanBandur <jordanbandur@hotmail.ca>
2024-04-08 12:34:28 -0700

    Add middleTest & refactor test code

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === false) {
-    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  } else {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = function(arr) {
   let midArray = [];
   let index;
@@ -36,11 +16,4 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
