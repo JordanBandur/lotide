f2161d67c222be98c9c531651f4bf493cac6f58b
JordanBandur <jordanbandur@hotmail.ca>
2024-04-08 14:47:13 -0700

    Remove import/export that aren't required

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
-const password = require('./password');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -30,10 +27,7 @@ module.exports = {
   eqObjects: eqObjects,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
-  flatten: flatten,
   letterPositions: letterPositions,
-  map: map,
-  password: password,
   takeUntil: takeUntil,
   without: without,
 };
