315713f04f68c91e50ddab5becd23446ccef0e1a
JordanBandur <jordanbandur@hotmail.ca>
2024-04-08 14:50:34 -0700

    Fix import/exports

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
+const flatten = require(`./flatten`);
 const letterPositions = require('./letterPositions');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
@@ -27,6 +28,7 @@ module.exports = {
   eqObjects: eqObjects,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
+  flatten: flatten,
   letterPositions: letterPositions,
   takeUntil: takeUntil,
   without: without,
